0e3d9c52805cd6602389e7d0cbe74276ab41d5ca
coutiwa <100156756+coutiwa@users.noreply.github.com>
2023-08-02 20:13:11 -0300

    Update game_page.js

--- game_page.js
+++ game_page.js
@@ -5,10 +5,10 @@ player1_score = 0;
 player2_score = 0;
 
 document.getElementById("player1_name").innerHTML = player1_name + " : ";
-document.getElementById("player2_name").innerHTMl = player2_name + " : ";
+document.getElementById("player2_name").innerHTML = player2_name + " : ";
 
-document.getElementById("player1_score").innerHTMl = player1_score;
-document.getElementById("player2_score").innerHTMl = player2_score;
+document.getElementById("player1_score").innerHTML = player1_score;
+document.getElementById("player2_score").innerHTML = player2_score;
 
 document.getElementById("player_question").innerHTML = "turno de pergunta: " + player1_name;
 document.getElementById("player_answer").innerHTML = "turno de resposta: " + player2_name;
